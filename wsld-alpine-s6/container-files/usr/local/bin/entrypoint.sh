07c77c2128c2dd7b63b2c9cbe9e7ad85bb5faab7
dennischan <48424802+dennischancs@users.noreply.github.com>
2021-07-21 17:25:11 +0800

    Update entrypoint.sh

--- wsld-alpine-s6/container-files/usr/local/bin/entrypoint.sh
+++ wsld-alpine-s6/container-files/usr/local/bin/entrypoint.sh
@@ -2,17 +2,17 @@
 set -e
 
 #---- wsl setting，DNS no 172.28.160.1 ----
-rm -f /etc/wsl.conf \
-    && echo "[network]" > /etc/wsl.conf \
-    && echo "generateResolvConf = false" >> /etc/wsl.conf \
-    && chmod 644 /etc/wsl.conf
+rm -f /etc/wsl.conf
+echo "[network]" > /etc/wsl.conf
+echo "generateResolvConf = false" >> /etc/wsl.conf
+chmod 644 /etc/wsl.conf
 
-rm -f /etc/resolv.conf \
-    && echo "# /etc/resolv.conf" > /etc/resolv.conf \
-    && echo "nameserver 223.5.5.5" >> /etc/resolv.conf \
-    && echo "nameserver 223.6.6.6" >> /etc/resolv.conf \
-    # readonly & cannot del
-    && chattr +i /etc/resolv.conf # cannot work in docker
+rm -f /etc/resolv.conf
+echo "# /etc/resolv.conf" > /etc/resolv.conf
+echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+echo "nameserver 223.6.6.6" >> /etc/resolv.conf
+# readonly & cannot del
+chattr +i /etc/resolv.conf # cannot work in docker
 
 #---- `service docker start` in alpine----
 # Step1
