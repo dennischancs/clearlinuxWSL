e475a009090dd561472d45ba2b036030a3c85b79
dennischan <48424802+dennischancs@users.noreply.github.com>
2021-07-22 23:52:01 +0800

    Update wsldns.sh

--- clr-wsl-base/container-files/usr/bin/wsldns.sh
+++ clr-wsl-base/container-files/usr/bin/wsldns.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 set -e
 
-#---- wsl setting，DNS no 172.28.160.1 ----
-rm -f /etc/wsl.conf
-echo "[network]" > /etc/wsl.conf
-echo "generateResolvConf = false" >> /etc/wsl.conf
-chmod 644 /etc/wsl.conf
+#---- RESET wsl setting，DNS no 172.28.160.1 ----
+if test -f "/etc/wsl.conf"; then
+  cp -f /etc/wsl.conf /etc/wsl.conf.bak
+  rm -f /etc/wsl.conf
+  echo "[network]" > /etc/wsl.conf
+  echo "generateResolvConf = false" >> /etc/wsl.conf
+  chmod 644 /etc/wsl.conf
+fi
 
-chattr -i /etc/resolv.conf # cannot work in docker
-rm -f /etc/resolv.conf
-echo "# /etc/resolv.conf" > /etc/resolv.conf
-echo "nameserver 223.5.5.5" >> /etc/resolv.conf
-echo "nameserver 223.6.6.6" >> /etc/resolv.conf
-chmod 755 /etc/resolv.conf
-# readonly & cannot del
-chattr +i /etc/resolv.conf # cannot work in docker
+if test -f "/etc/resolv.conf"; then
+  cp -f /etc/resolv.conf /etc/resolv.conf.bak
+  chattr -i /etc/resolv.conf # cannot work in docker
+  rm -f /etc/resolv.conf
+  echo "# /etc/resolv.conf" > /etc/resolv.conf
+  echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+  echo "nameserver 223.6.6.6" >> /etc/resolv.conf
+  chmod 644 /etc/resolv.conf
+  # readonly & cannot del
+  chattr +i /etc/resolv.conf # cannot work in docker
+fi
 
 exec $@
